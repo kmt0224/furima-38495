fb8ef87c97666cf22267f7fb0873c69b9f34735d
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2022-12-22 06:13:50 +0900

    ビュー編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -11,4 +12,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :first_name, :family_name, :kana_first_name, :kana_family_name, :birthday])
+  end
 end
