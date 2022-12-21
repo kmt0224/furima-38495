852c34f06718f7a468a46022137a4cc46fb0c46f
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2022-12-22 06:13:39 +0900

    userモデルのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nick_name,        presence: true
-  validates :first_name,       presence: true
-  validates :family_name,      presence: true
-  validates :kana_first_name,  presence: true
-  validates :kana_family_name, presence: true
-  validates :birthday,         presence: true
+  validates :nick_name,          presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers"}
+  validates :first_name,         presence: true
+  validates :family_name,        presence: true
+  validates :kana_first_name,    presence: true
+  validates :kana_family_name,   presence: true
+  validates :birthday,           presence: true
+
 end
