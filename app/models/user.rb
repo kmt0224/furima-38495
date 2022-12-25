ccab93e5c46a19a8f91825b6145b80921c71235e
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2022-12-25 21:54:06 +0900

    userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nick_name,          presence: true
+         
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers"}
-  validates :first_name,         presence: true
-  validates :family_name,        presence: true
-  validates :kana_first_name,    presence: true
-  validates :kana_family_name,   presence: true
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/
+
+  validates :nick_name,          presence: true
+  validates :password,           format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers"}
+  validates :first_name,         presence: true, format: { with: VALID_NAME_REGEX, message: "全角（漢字・ひらがな・カタカナ）で入力してください"}
+  validates :family_name,        presence: true, format: { with: VALID_NAME_REGEX, message: "全角（漢字・ひらがな・カタカナ）で入力してください"}
+  validates :kana_first_name,    presence: true, format: { with: VALID_KANA_NAME_REGEX, message: "全角（カタカナ）で入力してください"}
+  validates :kana_family_name,   presence: true, format: { with: VALID_KANA_NAME_REGEX, message: "全角（カタカナ）で入力してください"}
   validates :birthday,           presence: true
 
 end
