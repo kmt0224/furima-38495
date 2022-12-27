0cac6c2ad61866076fc21e73a967b31a816fbc49
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2022-12-28 02:02:36 +0900

    userモデルのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]/
   VALID_KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/
 
   validates :nick_name,          presence: true
