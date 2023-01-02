5eb00a82e7a34a194d893a83d4cc8ea405e32cdd
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-01-02 23:11:06 +0900

    ログイン画面への強制遷移実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+before_action :move_to_new_user_session, except: [:index]
+
   def index
     @items = Item.includes(:user)
   end
@@ -20,4 +22,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item, :explanation, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :date_of_shipment_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_new_user_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
