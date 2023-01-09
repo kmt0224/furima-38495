f880d0c88bd59500d9050cb769801383a508485a
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-01-09 21:49:13 +0900

    商品一覧の表示順変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_new_user_session, except: [:index]
 
   def index
-    @items = Item.includes(:user)
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
