f1b6d011c84fa86567dfe6760e5fdd4ca67575c8
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-01-09 16:51:45 +0900

    priceカラムのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,6 +17,5 @@ class Item < ApplicationRecord
   validates :shipping_fee_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id,       presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :date_of_shipment_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price,               presence: true, format: { with: /\A[0-9]+\z/ },
-                                  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price,               presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
