7c9757849ba9f5c9d10610879d338b7e1121444e
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-01-09 17:41:39 +0900

    itemモデルのfactorybot修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    image               { Faker::Lorem.sentence }
     item                { Faker::Lorem.sentence }
     explanation         { Faker::Lorem.sentence }
     category_id         { Faker::Number.between(from: 2, to: 11) }
@@ -10,5 +9,10 @@ FactoryBot.define do
     date_of_shipment_id { Faker::Number.between(from: 2, to: 4) }
     price               { Faker::Number.between(from: 300, to: 9_999_999) }
     association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
