2fe926e4cdb793cdae471465aadd7617f9cb781c
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-01-15 11:25:49 +0900

    before_actionの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,7 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     redirect_to action: :index unless @item.user_id == current_user.id
   end
 
