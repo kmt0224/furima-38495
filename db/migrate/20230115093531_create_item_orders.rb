468b493602e29483d9972cce0303d0add7660a81
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-01-15 18:53:35 +0900

    item_ordersテーブルの実装

--- db/migrate/20230115093531_create_item_orders.rb
+++ db/migrate/20230115093531_create_item_orders.rb
@@ -1,7 +1,8 @@
 class CreateItemOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :item_orders do |t|
-
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
       t.timestamps
     end
   end
