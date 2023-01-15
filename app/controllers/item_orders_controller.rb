9a2ad4dcffc3a8374ba5c1f7f47ed7fec738eae9
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-01-16 01:30:03 +0900

    before_action定義

--- app/controllers/item_orders_controller.rb
+++ app/controllers/item_orders_controller.rb
@@ -1,5 +1,20 @@
 class ItemOrdersController < ApplicationController
+  before_action :move_to_new_user_session
+  before_action :move_to_item_index
   def index
     @item = Item.find(params[:item_id])
   end
+
+  private
+  def move_to_new_user_session
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def move_to_item_index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
 end
