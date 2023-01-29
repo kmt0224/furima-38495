490fe296857dc2441b3dbe8c186f3898dd6e32fa
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-01-29 20:48:00 +0900

    商品情報と住所の保存実装

--- app/controllers/item_orders_controller.rb
+++ app/controllers/item_orders_controller.rb
@@ -5,6 +5,16 @@ class ItemOrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def create
+    @item_order = ItemOrder.new(
+      user_id: current_user.id,
+      item_id: params[:item_id]
+    )
+    @item_order.save
+    Address.create(address_params)
+    redirect_to root_path
+  end
+
   private
   def move_to_new_user_session
     redirect_to new_user_session_path unless user_signed_in?
@@ -15,6 +25,10 @@ class ItemOrdersController < ApplicationController
     if current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(item_order_id: @item_order.id)
   end
 
 end
