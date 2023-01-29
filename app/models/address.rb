9502ee2c81accb05a9283cee53428790e74ccece
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-01-29 20:49:15 +0900

    バリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,12 @@ class Address < ApplicationRecord
   belongs_to :item_order
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+    validates :municipality
+    validates :house_number
+    validates :phone_number, {with: /\d{10,11}/}
+  end
 end
