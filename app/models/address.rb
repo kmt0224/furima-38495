81524d7c159f325f89fb9591531c7b29352ecedc
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-01-29 22:01:25 +0900

    formオブジェクトパターンの実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,11 +3,4 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
-  with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
-    validates :municipality
-    validates :house_number
-    validates :phone_number, {with: /\d{10,11}/}
-  end
 end
