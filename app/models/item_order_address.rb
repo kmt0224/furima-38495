9c2779eae9c095ff0ce44701135a47ca4c7d529b
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-02-08 06:53:22 +0900

    バリデーション修正

--- app/models/item_order_address.rb
+++ app/models/item_order_address.rb
@@ -6,10 +6,11 @@ class ItemOrderAddress
     validates :user_id
     validates :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "Prefecture Select"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipality
     validates :house_number
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message:"is invalid."}
+    validates :token
   end
 
   def save
