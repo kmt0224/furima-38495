18bef3fc36401d00dc566c34a12193e40e9469a3
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-02-09 03:55:29 +0900

    item_order_addressモデル修正

--- app/models/item_order_address.rb
+++ app/models/item_order_address.rb
@@ -3,6 +3,7 @@ class ItemOrderAddress
   attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
+    validates :token
     validates :user_id
     validates :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -10,7 +11,6 @@ class ItemOrderAddress
     validates :municipality
     validates :house_number
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message:"is invalid."}
-    validates :token
   end
 
   def save
