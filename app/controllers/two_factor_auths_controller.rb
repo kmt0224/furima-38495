3f90cfbb7f704aebb5f9d28ad7eb6562634a0ad9
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-07-14 15:59:39 +0900

    two_factor_auths_controllerの作成と編集

---
+++ app/controllers/two_factor_auths_controller.rb
@@ -0,0 +1,51 @@
+class TwoFactorAuthsController < ApplicationController
+  def new
+    unless current_user.otp_secret
+      current_user.otp_secret = User.generate_otp_secret(32)
+      current_user.save!
+    end
+
+    @qe_code = build_qr_code
+  end
+  def create
+    if current_user.validate_and_consume_otp!(params[:otp_attempt])
+      current_user.otp_required_for_login = true
+      current_user.save!
+
+      redirect_to root_path
+
+    else
+      @error = 'Invalid pin code'
+      @qr_code = build_qr_code
+
+      render 'new'
+    end
+  end
+  def show
+    @error = 'Invalid pin code'
+    @qr_code = build_qr_code
+  end
+  def destroy
+    current_user.update_attributes(
+      otp_required_for_login:    false,
+      encrypted_otp_secret:      nil,
+      encrypted_otp_secret_iv:   nil,
+      encrypted_otp_secret_salt: nil,
+    )
+    redirect_to root_path
+  end
+
+  private
+  def build_qr_code
+    label = "DeviseTwo2"
+    issuer ="#{current_user[:email]}"
+    uri = current_user.otp_provisioning_uri(label, issuer: issuer)
+    qrcode = RQRCode::QRCode.new(uri)
+    qrcode.as_svg(
+      offset: 0,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 2
+    ).html_safe
+  end
+end
