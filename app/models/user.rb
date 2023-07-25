901121e671e90268571901cfa16605b36af2010d
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-07-25 16:54:45 +0900

    バックアップコードの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   devise :two_factor_authenticatable,
          :otp_secret_encryption_key => ENV['OTP_SECRET_KEY']
 
+  devise :two_factor_backupable, otp_number_of_backup_codes: 10 
+  serialize :otp_backup_codes, JSON
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :registerable,
