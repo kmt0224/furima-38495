5329f5611f166a7f9a032a80f7b400f034927bee
kmt0224 <kamacho.1997224@ezweb.ne.jp>
2023-07-26 18:03:23 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  devise_scope :user do
+    patch '/users/sign_in', to: 'users/sessions#create'
+    get '/users/two_factor', to: 'users/sessions#new_two_factor'
+    post '/users/two_factor', to: 'users/sessions#create_two_factor'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
